767fdd05b47407424c3ede6d63426e048d814d4f
Kamil Osak <32075064+kmlhombre@users.noreply.github.com>
2021-02-06 14:37:54 +0100

    ANNIHILATION

--- app/forms.py
+++
@@ -1,5 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextAreaField
-
-
-class GradeForm(FlaskForm):
--- config.py
+++
@@ -1,3 +0,0 @@
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@192.168.100.26:5432/elearningDB'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
